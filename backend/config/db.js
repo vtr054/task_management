ffa07f9f400db86259560a5ee74018b143150667
vtr054 <tharunvempati18@gmail.com>
2026-02-06 00:30:26 +0530

    Deployment updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,16 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql', 
+    dialect: 'mysql',
     logging: false,
+    ...(process.env.NODE_ENV === 'production' ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    } : {})
   }
 );
 
